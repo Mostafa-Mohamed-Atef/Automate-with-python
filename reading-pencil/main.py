64117fe2ebc754e05de9c531224fe7ef394757ff
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-09-02 20:04:06 +0300

    add with statement

--- reading-pencil/main.py
+++ reading-pencil/main.py
@@ -6,12 +6,12 @@ keyboard_controller = keyboard.Controller()
 def track(key):
     if key == keyboard.Key.right:
         try:
-            keyboard_controller.press(keyboard.Key.ctrl)
-            keyboard_controller.press(keyboard.Key.shift)
-            keyboard_controller.press(keyboard.Key.right)
-            keyboard_controller.release(keyboard.Key.ctrl)
-            keyboard_controller.release(keyboard.Key.shift)
-            keyboard_controller.release(keyboard.Key.right)
+            with keyboard_controller.ctrl_pressed(keyboard.Key.ctrl):
+                keyboard_controller.press(keyboard.Key.shift)
+                keyboard_controller.press(keyboard.Key.right)
+                # keyboard_controller.release(keyboard.Key.ctrl)
+                # keyboard_controller.release(keyboard.Key.shift)
+                keyboard_controller.release(keyboard.Key.right)
         except Exception as e:
             print(e)
     if key == keyboard.Key.esc:
@@ -19,7 +19,7 @@ def track(key):
         return False
 
 
-with keyboard.Listener(on_press=track) as listener:
+with keyboard.Listener(on_release=track) as listener:
     listener.join()
 
 # keyboard_controller.type("hello world")
