dcadbf810a512fe808b675a95d7b84ad296965d1
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-09-05 17:48:52 +0300

    youtube to mp3

---
+++ YT2MP3/main.py
@@ -0,0 +1,24 @@
+from pytubefix import YouTube
+from tqdm import tqdm
+
+progress_bar = None
+def download():
+    global progress_bar
+    try:
+       audio_stream = yt.streams.filter(adaptive=True, file_extension='mp4', only_audio=True).order_by('abr').desc().first()
+       print(yt.title)
+       progress_bar = tqdm(total=audio_stream.filesize, unit='0', unit_scale=True, desc="Downloading")
+       audio_stream.download(filename=yt.title)
+       progress_bar.close()
+       print('completed')
+    except Exception as e:
+        print(e)
+def progress(stream, chunk, bytes_remaining):
+    global progress_bar
+    file_size = stream.filesize
+    bytes_downloaded = file_size - bytes_remaining
+    progress_bar.update(len(chunk))
+
+url = input("Import your link: \n")
+yt = YouTube(url, on_progress_callback=progress)
+download()
