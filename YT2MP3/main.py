60760af26d362f03d121fba10bc7144f022e53a7
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-09-06 21:01:36 +0300

    fixed file

--- YT2MP3/main.py
+++ YT2MP3/main.py
@@ -2,17 +2,19 @@ from pytubefix import YouTube
 from tqdm import tqdm
 
 progress_bar = None
+
 def download():
     global progress_bar
     try:
        audio_stream = yt.streams.filter(adaptive=True, file_extension='mp4', only_audio=True).order_by('abr').desc().first()
        print(yt.title)
        progress_bar = tqdm(total=audio_stream.filesize, unit='0', unit_scale=True, desc="Downloading")
-       audio_stream.download(filename=yt.title)
+       audio_stream.download(filename=f"{yt.title}.mp3")
        progress_bar.close()
        print('completed')
     except Exception as e:
         print(e)
+
 def progress(stream, chunk, bytes_remaining):
     global progress_bar
     file_size = stream.filesize
