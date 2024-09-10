fcf042c14e19e477dc2519a54f38551108266e75
مُصْطَفى <151864798+Mostafa-Mohamed-Atef@users.noreply.github.com>
2024-09-10 17:40:48 +0300

    Update main.py

--- Image-cropper/main.py
+++ Image-cropper/main.py
@@ -3,3 +3,5 @@ original_image = Image.open("my_image.jpeg")
 import numpy as np
 image_array = np.array(original_image)
 cropped_image = Image.image_transforms.center_crop(image=np.ndarray,size=(1000, 1000))
+imgplot = plt.imshow(cropped_image)
+plt.show()
