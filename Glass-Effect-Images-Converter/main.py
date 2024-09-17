967df3583f7b6c29ac5205badedbff2aba7b0c87
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-09-17 14:06:50 +0300

    add save

--- Glass-Effect-Images-Converter/main.py
+++ Glass-Effect-Images-Converter/main.py
@@ -4,9 +4,10 @@ from PIL import Image, ImageFilter
 
 
 # creating a image object 
-im1 = Image.open(r"test.jpg") 
+im1 = Image.open(r"neon-john-wick-5a5e1t8lfgff7d73.jpg") 
 
 # applying the Gaussian Blur filter 
-im2 = im1.filter(ImageFilter.GaussianBlur(radius = 20)) 
+im2 = im1.filter(ImageFilter.GaussianBlur(radius = 5)) 
 
 im2.show() 
+im2.save("output.jpg")
