90102b0e62c887090d23dcf34a4649d39013aafb
مُصْطَفى <151864798+Mostafa-Mohamed-Atef@users.noreply.github.com>
2024-09-27 20:25:36 +0300

    Add img2pdf

--- pdf2image/main.py
+++ pdf2image/main.py
@@ -5,3 +5,7 @@ pages = convert_from_path('name.pdf')
 
 for page in pages:
     page.save('output.jpg', 'JPEG')
+#EDIT THIS FOR CONVERTING IMG TO PDF
+import img2pdf
+with open("output.pdf", "wb") as file:
+   file.write(img2pdf.convert([i for i in os.listdir('path to image') if i.endswith(".jpg")]))
