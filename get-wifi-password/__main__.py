ab85dd79d7f5b6025efe55d2bb95252b5ff75f99
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-10-02 22:08:34 +0300

    qr generated password

--- get-wifi-password/__main__.py
+++ get-wifi-password/__main__.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
-
+import qrcode
+from PIL import Image
 def get_wifi_password():
     try:
         # Get the active SSID using nmcli command
@@ -28,6 +29,7 @@ def get_wifi_password():
         # Print the SSID and password
         print(f"SSID: {ssid}")
         print(f"Password: {password}")
+        return password
     except subprocess.CalledProcessError as e:
         # Handle errors when executing subprocess commands
         print(f"Error executing command: {e}")
@@ -38,5 +40,22 @@ def get_wifi_password():
         # Handle any other unexpected errors
         print(f"An unexpected error occurred: {e}")
 
+def generate_qrcode(password):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(password)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    img.show()
+
+
 if __name__ == "__main__":
-    get_wifi_password()
+    password = get_wifi_password()
+    qr = input("Generate qr code?\n").lower()
+    if qr == 'y': generate_qrcode(password)
